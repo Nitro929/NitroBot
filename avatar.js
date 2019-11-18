4529b6b5dfecf5d0dbc1f027d0cd97036abab70f
Nitro929 <57894128+Nitro929@users.noreply.github.com>
2019-11-18 20:32:53 +0800

    Add files via upload

---
+++ avatar.js
@@ -0,0 +1,27 @@
+/**
+ * Send a user a link to their avatar
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is n!avatar
+  if (message.content === 'n!avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
+});
+
