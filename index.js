eff5bc9c52eeecdebe29e4ef5bfae4cf71744d5d
Nitro929 <57894128+Nitro929@users.noreply.github.com>
2019-11-18 18:31:08 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
+const { Client , Collection , RichEmbed } = require("discord.js")
 const Discord = require("discord.js");
-const bot = new Discord.Client();
+const bot = new Client();
 const config = require("./config.json");
 const token = require("./config.json");
-const {Token} = require("botconfig.json");
-bot.on('ready' , ()=> {
-  console.log(`Bot Logging At Discord ${bot.user.tag}!`);
-});
+["aliases", "commands"].forEach(x => bot[x] = new Collection());
+["command", "events"].forEach(x => require(`./handlers/${x}`) (bot));
 
-bot.on('message' , msg =>{
-  if (msg = content === 'ping'){
-      msg.reply("Pong!");
-  }
-});
 
-bot.login(config.token)
-bot.login(Token)
+bot.login(config.token);
